10c3ad02131065c7639229a8fdb4b744bfcd1e4e
utibe inyangetuk <inyangutibz@gmail.com>
2024-05-20 16:38:20 +0100

    changed client's section background color and fixed flex wrapping

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,10 +2,8 @@
 	<currentPage />
 	<div class="wrapper">
 		<p>
-			COBEF INTERNATIONAL LIMITED retains quality delivery and clients satisfaction as its driving force. Based on
-			ISO certification, we are guided
-			by the highest safety and quality standards to ensure that our clients receive the highest levels of
-			satisfaction in addition to the
+			COBEF INTERNATIONAL LIMITED retains quality delivery and clients satisfaction as its driving force. Based on ISO certification, we are guided
+			by the highest safety and quality standards to ensure that our clients receive the highest levels of satisfaction in addition to the
 			commercial experience necessary to address technically challenging projects.
 		</p>
 		<div class="projects">
@@ -30,40 +28,52 @@
 		<div class="clients">
 			<h2>Our clients</h2>
 			<div class="client-container">
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Hydrocarbon Pollution Remediation Project (HYPREP)</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/darycet.webp" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/darycet.webp" alt="company logo" />
 					<li>Darycent International</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/briscoeGroup.jpg" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/briscoeGroup.jpg" alt="company logo" />
 					<li>Briscoe Group Limited</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Ministry of Water and Rural Development Rivers State</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/shell.jpg" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/shell.jpg" alt="company logo" />
 					<li>Shell Petroleum Development Company</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/julius berger.jpg" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/julius berger.jpg" alt="company logo" />
 					<li>Julius Berger</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/kinetic.png" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/kinetic.png" alt="company logo" />
 					<li>Kinetic Electromechanic Nigeria Limited</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Top Engineering International</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Felian Ventures</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Cylon Nigeria Limited</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Altruist Ideals</li>
 				</div>
-				<div class="client-item"><img src="../assets/client logos/#" alt="company logo">
+				<div class="client-item">
+					<img src="../assets/client logos/#" alt="company logo" />
 					<li>Alcon Nigeria Limited</li>
 				</div>
 			</div>
@@ -73,107 +83,111 @@
 </template>
 
 <script setup>
-import currentPage from '../components/CurrentPage.vue';
-import Footer from '../components/Footer.vue';
+	import currentPage from '../components/CurrentPage.vue';
+	import Footer from '../components/Footer.vue';
 </script>
 <style scoped>
-.wrapper {
-	padding: 20px;
-}
+	.wrapper {
+		padding: 20px;
+	}
 
-.wrapper P {
-	letter-spacing: 1.3px;
-	line-height: 2em;
-	font-size: 17px;
-	font-weight: 400;
-	margin-bottom: 20px;
-}
+	.wrapper P {
+		letter-spacing: 1.3px;
+		line-height: 2em;
+		font-size: 17px;
+		font-weight: 400;
+		margin-bottom: 20px;
+	}
 
-.projects {
-	border: 1px solid;
-	border-radius: 5px;
-	padding: 10px;
-	background-color: #020e2a;
-	color: #ffffff;
-	margin-bottom: 2em;
-}
+	.projects {
+		border: 1px solid;
+		border-radius: 5px;
+		padding: 10px;
+		background-color: #020e2a;
+		color: #ffffff;
+		margin-bottom: 2em;
+	}
 
-.projects h2 {
-	color: orange;
-	text-transform: uppercase;
-	letter-spacing: 2px;
-	text-align: center;
-}
+	.projects h2 {
+		color: orange;
+		text-transform: uppercase;
+		letter-spacing: 2px;
+		text-align: center;
+	}
 
-.projects ul {
-	margin-left: 30px;
-}
+	.projects ul {
+		margin-left: 30px;
+	}
 
-.projects li {
-	list-style-type: disc;
-	letter-spacing: 1.3px;
-	line-height: 3em;
-	font-size: 14px;
-	text-transform: uppercase;
-}
+	.projects li {
+		list-style-type: disc;
+		letter-spacing: 1.3px;
+		line-height: 3em;
+		font-size: 14px;
+		text-transform: uppercase;
+	}
 
-.gallery {
-	border: 1px solid;
-	border-radius: 5px;
-	margin-bottom: 2em;
-	background-color: #020e2a;
-}
+	.gallery {
+		border: 1px solid;
+		border-radius: 5px;
+		margin-bottom: 2em;
+		background-color: #020e2a;
+	}
 
-.gallery h2 {
-	text-align: center;
-	color: orange;
-	letter-spacing: 2px;
-	font-size: 35px;
-	text-transform: uppercase;
-}
+	.gallery h2 {
+		text-align: center;
+		color: orange;
+		letter-spacing: 2px;
+		font-size: 35px;
+		text-transform: uppercase;
+	}
 
-.clients {
-	margin-top: 0.5em;
-	border: 1px solid;
-	border-radius: 5px;
-	background-color: #020e2a;
-}
+	.clients {
+		margin-top: 0.5em;
+		/*border: 1px solid;*/
+		border-radius: 5px;
+		/*background-color: #020e2a;*/
+	}
 
-.clients h2 {
-	text-align: center;
-	color: orange;
-	letter-spacing: 2px;
-	font-size: 35px;
-	text-transform: uppercase;
-	margin-bottom: 0.5em;
-}
+	.clients h2 {
+		text-align: center;
+		color: orange;
+		letter-spacing: 2px;
+		font-size: 35px;
+		text-transform: uppercase;
+		margin-bottom: 0.5em;
+	}
 
-.client-container {
-	display: flex;
-	flex-wrap: wrap;
-	gap: 20px;
-	padding: 10px;
-	border-radius: 5px;
-}
+	.client-container {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 20px;
+		justify-content: flex-start;
+	}
 
-.client-item {
-	display: flex;
-	align-items: center;
-	border: 1px solid #ffffff;
-	padding: 10px;
-	border-radius: 5px;
-}
+	.client-item {
+		display: flex;
+		align-items: center;
+		flex: 1 1 auto;
+		padding: 10px;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+	}
 
-.client-item img {
-	height: 40px;
-	width: 40px;
-	margin-right: 10px;
-	border: 1px solid #ffffff7d;
-	border-radius: 50px;
-}
+	.client-item img {
+		max-width: 50px;
+		margin-right: 10px;
+	}
 
-.client-item li {
-	color: #ffffffaa;
-	letter-spacing: 1px;
-}
+	.client-item li {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	@media only screen and (max-width: 600px) {
+		.wrapper {
+			padding: 10px;
+		}
+	}
 </style>
