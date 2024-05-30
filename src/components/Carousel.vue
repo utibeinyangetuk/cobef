a42606c29bd67ff02d0f405dff32fd4d2230b6be
utibe inyangetuk <inyangutibz@gmail.com>
2024-05-30 15:30:08 +0100

    added left and right button for optional scrolling

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,18 +1,17 @@
 <template>
 	<div class="carousel">
+		<!-- Left arrow -->
+		<button class="carousel-arrow uil uil-arrow-left" @click="prev" />
 		<div class="carousel-inner" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
 			<div v-for="(image, index) in images" :key="index" class="carousel-slide">
 				<img :src="image" alt="Carousel Slide" class="carousel-image" />
 			</div>
 		</div>
+		<!-- Right arrow -->
+		<button class="carousel-arrow uil uil-arrow-right" @click="next" />
+
 		<div class="indicators">
-			<span
-				v-for="(image, index) in images"
-				:key="index"
-				:class="{ active: index === currentIndex }"
-				@click="goTo(index)"
-				class="indicator"
-			></span>
+			<span v-for="index in images" :key="index" :class="{ active: index === currentIndex }" @click="goTo(index)" class="indicator"></span>
 		</div>
 	</div>
 </template>
@@ -33,9 +32,9 @@
 		currentIndex.value = (currentIndex.value + 1) % images.value.length;
 	};
 
-	// const prev = () => {
-	// 	currentIndex.value = (currentIndex.value - 1 + images.value.length) % images.value.length;
-	// };
+	const prev = () => {
+		currentIndex.value = (currentIndex.value - 1 + images.value.length) % images.value.length;
+	};
 
 	const goTo = (index) => {
 		currentIndex.value = index;
@@ -100,6 +99,31 @@
 	.carousel-image {
 		width: 100%;
 		height: 100%;
+	}
+
+	.carousel-arrow {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		background-color: #000;
+		border-radius: 5px;
+		border: none;
+		color: white;
+		font-size: 2.4rem;
+		cursor: pointer;
+		z-index: 1000;
+		opacity: 0.7;
+	}
+	.uil-arrow-left {
+		left: 10px;
+	}
+
+	.uil-arrow-right {
+		right: 10px;
+	}
+
+	.carousel-arrow:hover {
+		color: #ddd;
 	}
 
 	/** media query **/
