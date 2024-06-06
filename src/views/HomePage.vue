16c7e9be88b14d5bed76b46d24fcf1c66866b236
utibe inyangetuk <inyangutibz@gmail.com>
2024-06-06 14:45:15 +0100

    Possible final design for homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,89 +1,111 @@
-<template>
-   <div class="container">
-      <div class="miniHeader">
-         <div class="contact">
-            <i class="uil uil-chat" /><span> 24x7 Live Support </span>
-            <i class="uil uil-phone" /><span>(+234) xxx xxx xxxx || (+234) xxx xxx xxxx</span>
-         </div>
-         <div class="social">
-            <span>Follow us </span><i class="uil uil-twitter" /><i class="uil uil-facebook-f" /><i
-               class="uil uil-linkedin"
-            /><i class="uil uil-whatsapp" />
-         </div>
-      </div>
-      <NavBar />
-      <Carousel />
-      <History />
-      <Footer />
-   </div>
-</template>
-
 <script setup>
-import Carousel from '../components/Carousel.vue'
-import Footer from '../components/Footer.vue'
-import History from '../components/History.vue'
-import NavBar from '../components/NavBar.vue'
+	import CurrentPage from '@/components/CurrentPage.vue';
+	import Footer from '@/components/Footer.vue';
+	import ImageSlot from '@/slots/Image-slot.vue';
+	import { useRouter } from 'vue-router';
+	const router = useRouter();
+	const navigateTo = (path) => router.push(path);
 </script>
 
+<template>
+	<CurrentPage />
+	<div class="main-wrapper">
+		<ImageSlot class="container1">
+			<template v-slot:title>
+				<h2>Our workforce</h2>
+			</template>
+			<template v-slot:button1>
+				<button @click="navigateTo('/products')" class="button1">View More</button>
+			</template>
+			<template v-slot:button2>
+				<button @click="navigateTo('/products')" class="button2">Order Now</button>
+			</template>
+		</ImageSlot>
+		<ImageSlot class="container1">
+			<template v-slot:title>
+				<h2>Our workforce</h2>
+			</template>
+			<template v-slot:button1>
+				<button @click="navigateTo('/products')" class="button1">View More</button>
+			</template>
+			<template v-slot:button2>
+				<button @click="navigateTo('/products')" class="button2">Order Now</button>
+			</template>
+		</ImageSlot>
+		<ImageSlot class="container1">
+			<template v-slot:title>
+				<h2>Our workforce</h2>
+			</template>
+			<template v-slot:button1>
+				<button @click="navigateTo('/products')" class="button1">View More</button>
+			</template>
+			<template v-slot:button2>
+				<button @click="navigateTo('/products')" class="button2">Order Now</button>
+			</template>
+		</ImageSlot>
+		<ImageSlot class="container1">
+			<template v-slot:title>
+				<h2>Our workforce</h2>
+			</template>
+			<template v-slot:button1>
+				<button @click="navigateTo('/products')" class="button1">View More</button>
+			</template>
+			<template v-slot:button2>
+				<button @click="navigateTo('/products')" class="button2">Order Now</button>
+			</template>
+		</ImageSlot>
+	</div>
+	<Footer />
+</template>
 <style scoped>
-.container {
-   height: 100%;
-   background-color: #fdf5e5;
-}
+	.container1 {
+		/* background-image: url(../assets/projects/three.jpg);
+  background-repeat: no-repeat;
+  background-size: cover; /* Change from 'cover' to 'contain' */
+		background-position: center; /* Optional: Center the image */
+		height: 100vh; /* Set height to cover the entire viewport */
+		position: relative;
+	}
+	h2 {
+		letter-spacing: 2px;
+		font-size: 30px;
+		text-transform: capitalize;
+		margin-top: 100px;
+	}
+	button {
+		padding: 10px 80px;
+		border-radius: 5px;
+		border: 1px solid gray;
+		letter-spacing: 2px;
+		cursor: pointer;
+		background: transparent;
+		font-weight: bold;
+		font-size: 17px;
+	}
+	.button1 {
+		background-color: #fff;
+		color: #000;
+	}
+	.button1:hover {
+		background-color: #000;
+		color: #fff;
+	}
+	.button2 {
+		background-color: #000;
+		color: #fff;
+	}
+	.button2:hover {
+		background-color: #fff;
+		color: #000;
+	}
 
-.miniHeader {
-   background-color: #020e2a;
-   height: 40px;
-   color: #ffffff;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   padding: 10px;
-}
-
-.contact {
-   padding: 9x;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-}
-
-.contact i {
-   color: #fab60f;
-   font-size: 20px;
-   margin-right: 6px;
-}
-
-.contact span {
-   margin-right: 10px;
-   font-size: 12px;
-}
-
-.social {
-   padding: 5px;
-   display: flex;
-   align-items: center;
-}
-
-.social i {
-   margin-right: 10px;
-   font-size: 20px;
-}
-
-.social i:hover {
-   color: #fab60f;
-   cursor: pointer;
-}
-
-.social span {
-   color: #fab60f;
-   font-weight: bold;
-   letter-spacing: 2px;
-   margin-right: 10px;
-}
-
-.history {
-   border: 2px solid #000;
-   height: 500px;
-}
+	@media screen and (max-width: 600px) {
+		button {
+			font-size: 15px;
+			padding: 14px;
+		}
+		.container1 {
+			height: 60vh;
+		}
+	}
 </style>
