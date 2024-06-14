2572ba322aacbca48364db9ad1c904c3780f0264
utibe inyangetuk <inyangutibz@gmail.com>
2024-06-14 15:04:55 +0100

    added space mono font

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
 #app {
 	font-family: 'Montserrat', sans-serif;
