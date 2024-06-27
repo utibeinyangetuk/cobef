cd62879253cb4aad652c53979885f26289bf364e
utibe inyangetuk <inyangutibz@gmail.com>
2024-06-27 09:00:20 +0100

    Fixed carousel for desktop

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,138 +1,102 @@
 <template>
 	<div class="carousel">
-		<!-- Left arrow -->
-		<button class="carousel-arrow uil uil-arrow-left" @click="prev" />
 		<div class="carousel-inner" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
-			<div v-for="(image, index) in images" :key="index" class="carousel-slide">
-				<img :src="image" alt="Carousel Slide" class="carousel-image" />
+			<div class="carousel-item" v-for="(image, index) in images" :key="index">
+				<img :src="image" :alt="'Slide ' + index" />
 			</div>
 		</div>
-		<!-- Right arrow -->
-		<button class="carousel-arrow uil uil-arrow-right" @click="next" />
-
-		<div class="indicators">
-			<span v-for="index in images" :key="index" :class="{ active: index === currentIndex }" @click="goTo(index)" class="indicator"></span>
+		<button class="carousel-control prev" @click="prevSlide">&#10094;</button>
+		<button class="carousel-control next" @click="nextSlide">&#10095;</button>
+		<div class="carousel-indicators">
+			<span v-for="(image, index) in images" :key="index" class="indicator" :class="{ active: index === currentIndex }" @click="goToSlide(index)"></span>
 		</div>
 	</div>
 </template>
 
 <script setup>
-	import { onUnmounted, ref, toRefs } from 'vue';
-
+	import { ref } from 'vue';
 	const props = defineProps({
 		images: {
 			type: Array,
 			required: true,
 		},
 	});
-	const { images } = toRefs(props);
 	const currentIndex = ref(0);
-
-	const next = () => {
-		currentIndex.value = (currentIndex.value + 1) % images.value.length;
-	};
-
-	const prev = () => {
-		currentIndex.value = (currentIndex.value - 1 + images.value.length) % images.value.length;
-	};
-
-	const goTo = (index) => {
-		currentIndex.value = index;
-	};
-
-	// Automatic sliding
+	const prevSlide = () => (currentIndex.value = currentIndex.value === 0 ? props.images.length - 1 : currentIndex.value - 1);
+	const nextSlide = () => (currentIndex.value = currentIndex.value === props.images.length - 1 ? 0 : currentIndex.value + 1);
+	const goToSlide = (index) => (currentIndex.value = index);
 	let timer;
-	const autoSlide = () => {
-		timer = setInterval(() => {
-			next();
-		}, 3000);
-	};
-
+	const autoSlide = () => (timer = setInterval(() => nextSlide(), 3000));
 	autoSlide();
-
-	onUnmounted(() => {
-		clearInterval(timer);
-	});
 </script>
 
 <style scoped>
-	.indicators {
+	.carousel {
+		position: relative;
+		width: 100%;
+    height: 90vh;
+		margin: auto;
+		overflow: hidden;
+	}
+
+	.carousel-inner {
+		display: flex;
+		transition: transform 0.5s ease-in-out;
+	}
+
+	.carousel-item {
+		min-width: 100%;
+		box-sizing: border-box;
+	}
+
+	.carousel-item img {
+		width: 100%;
+		height: 90vh;
+		display: block;
+    background-repeat: no-repeat;
+	}
+
+	.carousel-control {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		background-color: rgba(0, 0, 0, 0.5);
+		border: none;
+		color: white;
+		font-size: 20px;
+		cursor: pointer;
+		padding: 10px;
+		height: 50px;
+    width: 50px;
+    border-radius: 50%;
+	}
+
+	.carousel-control.prev {
+		left: 10px;
+	}
+
+	.carousel-control.next {
+		right: 10px;
+	}
+
+	.carousel-indicators {
 		position: absolute;
 		bottom: 10px;
-		left: 50%;
-		transform: translateX(-50%);
-		display: flex;
-		background-color: #000;
-		padding:5px;
-		border-radius: 10px;
+		width: 100%;
+		text-align: center;
 	}
 
 	.indicator {
+		display: inline-block;
 		width: 10px;
 		height: 10px;
 		margin: 0 5px;
-		background-color: rgba(255, 255, 255, 0.5);
+		background-color: rgba(0, 0, 0, 0.5);
 		border-radius: 50%;
 		cursor: pointer;
 	}
 
 	.indicator.active {
-		background-color:#fff;
-	}
-
-	.carousel {
-		position: relative;
-		overflow: hidden;
-		width: 100%;
-		height: 572px;
-	}
-
-	.carousel-inner {
-		display: flex;
-		transition: transform 0.5s ease;
-		height: 100%;
-	}
-
-	.carousel-slide {
-		min-width: 100%;
-		box-sizing: border-box;
-		height: 100%;
-	}
-
-	.carousel-image {
-		width: 100%;
-		height: 100%;
-	}
-
-	.carousel-arrow {
-		position: absolute;
-		top: 50%;
-		transform: translateY(-50%);
-		background-color: #000;
-		border-radius: 5px;
-		border: none;
-		color: white;
-		font-size: 2.4rem;
-		cursor: pointer;
-		z-index: 1000;
-		opacity: 0.7;
-	}
-	.uil-arrow-left {
-		left: 10px;
-	}
-
-	.uil-arrow-right {
-		right: 10px;
-	}
-
-	.carousel-arrow:hover {
-		color: #ddd;
-	}
-
-	/** media query **/
-	@media only screen and (max-width: 600px) {
-		.carousel {
-			height: 300px;
-		}
+		background-color: white;
 	}
 </style>
