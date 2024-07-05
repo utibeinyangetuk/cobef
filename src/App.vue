33ce6217588da851380c5c55e65eef01fe78b08c
utibe inyangetuk <inyangutibz@gmail.com>
2024-07-05 11:06:34 +0100

    Profile card and minor changes to entry file

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,6 @@ import CustomCursor from "./components/CustomCursor.vue";
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
-html {
-	scroll-behaviour: smooth;
-}
 body, a, * {
 	overflow: hidden;
 	cursor:none;
@@ -35,7 +32,7 @@ body, a, * {
 	list-style: none;
 
 	::-webkit-scrollbar {
-		width: 10px;
+		width: 0px;
 	}
 
 	::-webkit-scrollbar-track {
@@ -43,7 +40,7 @@ body, a, * {
 	}
 
 	::-webkit-scrollbar-thumb {
-		background: orange;
+		background: gray;
 		border-radius: 10px;
 	}
 }
