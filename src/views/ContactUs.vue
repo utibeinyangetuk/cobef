db06966d43e76db87b1d8b0427753465eff6ee3b
utibe inyangetuk <inyangutibz@gmail.com>
2024-07-11 03:39:41 +0100

    contact page

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,188 +1,108 @@
-<template>
-	<Currentpage />
-	<section>
-		<div class="email">
-			<p>Send us a mail</p>
-			<form action="#">
-				<input type="email" placeholder="your email" />
-				<textarea placeholder="your message goes here" cols="30" rows="14" />
-				<button type="submit">send email</button>
-			</form>
-		</div>
-		<div class="wrapper2">
-			<div class="contact">
-				<p>Phone</p>
-				<ul>
-					<li>(+234) xxx xxx xxxx</li>
-					<li>(+234) xxx xxx xxxx</li>
-					<li>(+234) xxx xxx xxxx</li>
-					<li>(+234) xxx xxx xxxx</li>
-					<li>(+234) xxx xxx xxxx</li>
-				</ul>
-			</div>
-			<div class="social">
-				<p>E-mail</p>
-				<ul>
-					<li>adminuser@email.com</li>
-					<li>adminuser@email.com</li>
-					<li>adminuser@email.com</li>
-				</ul>
-			</div>
-			<div class="location">
-				<p>Address</p>
-				<ul>
-					<li>Km 17 Port Harcourt/Aba Express Way P. O. Box, 2769 Port Harcourt.</li>
-				</ul>
-			</div>
-		</div>
-	</section>
-	<Footer />
-</template>
-
 <script setup>
-	import Currentpage from '../components/CurrentPage.vue';
+import CurrentPage from '@/components/CurrentPage.vue';
 import Footer from '@/components/Footer.vue';
 </script>
 
+<template>
+	<currentPage />
+	<div class="container">
+		<div class="header">
+			<p>Contact us</p>
+			<h2>Do you want to <a href="#">start a project</a>,<br> <a href="#">join the team</a> or just say hi?<br> we'd
+				love to hear from you.</h2>
+		</div>
+		<div class="office">
+			<p>Our Office</p>
+			<div class="information">
+				<img src="../assets/homepage-images/company.avif" />
+				<div class="details">
+					<h1>Port Harcourt, Nigeria</h1>
+					<h2>Km 17 Port Harcourt/Aba Express Way P. O. Box, 2769 Port Harcourt</h2>
+					<a href="mailto:info@cobefinternationalltd.com">info@cobefinternationalltd.com</a>
+					<a href="tel:+1234567890">+(234) 567-890-9123</a>
+					<a href="https://www.google.com/maps/place/Cobef+International+Limited/@4.8610877,7.0891647,17.42z/data=!4m9!1m2!2m1!1sKm+17+Port+Harcourt%2FAba+Express+Way+P.+O.+Box,+2769+Port+Harcourt!3m5!1s0x1069d2ad21ae86bd:0x46fb3e4446572965!8m2!3d4.860476!4d7.0880525!16s%2Fg%2F11hzhc6m0m?entry=ttu"
+						target="_blank">View in maps</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<Footer />
+</template>
 <style scoped>
-	section {
-		margin-top: 20px;
-		display: flex;
-		justify-content: space-between;
-		width: 100%;
-		padding: 30px;
-	}
+.container {
+	background-color: #10100E;
+	padding: 60px;
+}
 
-	section p {
-		padding: 5px;
-		border-bottom: 1px solid rgba(128, 128, 128, 0.263);
-		text-transform: capitalize;
+.header {
+	width: 70%;
+	color: #535349;
+	margin-bottom: 70px;
+}
+
+.header p {
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	font-size: 14px;
+	margin-bottom: 20px;
+	font-weight: 700;
+	color:#ffffe3
+}
+
+.header h2 {
+	font-weight: 400;
+	font-size: 45px;
+	letter-spacing: 1px;
+	line-height: 1.6em;
+}
+
+.header h2 a {
+	color: #ffffe3;
+	text-decoration: underline;
+	text-transform: uppercase;
+}
+
+.office {
+	border-top: 1px solid #535349;
+	border-bottom: 1px solid #535349;
+}
+
+.office p {
+	text-transform: uppercase;
 		letter-spacing: 2px;
-		background-color: #111111;
-		color: #ffffff;
-	}
+		font-size: 14px;
+		margin-bottom: 40px;
+		margin-top: 40px;
+		font-weight: 700;
+		color: #ffffe3
+}
 
-	.email {
-		border: 1px solid rgba(128, 128, 128, 0.263);
-		border-radius: 5px;
-		width: 50%;
-		height: 400px;
-	}
+.information {
+	display: flex;
+	gap: 40px;
+}
 
-	.wrapper2 {
-		width: 50%;
-		border-radius: 5px;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		align-items: flex-start;
-		margin-left: 10px;
-	}
+.information img {
+	height: 500px;
+	border-radius: 10px;
+	margin-bottom: 70px;
+}
 
-	.contact,
-	.social,
-	.location {
-		width: 100%;
-		border: 1px solid rgba(128, 128, 128, 0.263);
-		border-radius: 5px;
-	}
-
-	.contact ul {
-		padding: 5px;
-		line-height: 25px;
-	}
-
-	.social ul {
-		padding: 5px;
-		line-height: 25px;
-		list-style: disc;
-	}
-
-	.location ul {
-		padding: 5px;
-		line-height: 25px;
-		list-style: disc;
-	}
-
-	form {
-		display: flex;
-		flex-direction: column;
-		padding: 10px;
-		justify-content: center;
-	}
-
-	form input {
-		border-radius: 3px;
-		font-size: 13px;
-		height: 40px;
-		border: 1px solid gray;
-		margin-bottom: 10px;
-		padding: 5px;
-	}
-
-	form input::placeholder {
-		letter-spacing: 1px;
-		text-transform: capitalize;
-	}
-
-	form textarea {
-		border-radius: 5px;
-		padding: 3px;
-		border: 1px solid gray;
-		margin-bottom: 10px;
-	}
-
-	form textarea::placeholder {
-		letter-spacing: 1px;
-		text-transform: capitalize;
-	}
-
-	button {
-		height: 40px;
-		cursor: pointer;
-		background-color: #111111;
-		border: none;
-		color: #ffffff;
-		letter-spacing: 2px;
-		text-transform: uppercase;
-		font-size: 15px;
-		border-radius: 3px;
-		font-weight: bold;
-	}
-
-	button:hover {
-		background-color: #111111;
-	}
-
-	@media only screen and (max-width: 600px) {
-		section {
-			margin-top: 10px;
-			display: flex;
-			flex-direction: column;
-			padding: 5px;
-		}
-
-		.email {
-			width: 100%;
-			margin-bottom: 10px;
-			border: 1px solid;
-		}
-
-		.wrapper2 {
-			width: 100%;
-			margin: 0px;
-		}
-
-		.contact,
-		.social,
-		.location {
-			margin-bottom: 20px;
-			border: 1px solid;
-		}
-
-		.wrapper2 li {
-			font-size: small;
-			letter-spacing: 1.5px;
-		}
-	}
+.details {
+	display: flex;
+	flex-direction:column;
+	line-height: 3em;
+	color:#ffffe3;
+}
+.details h1{
+	margin-bottom: 20px;
+}
+.details h2{
+	font-size: 1rem;
+}
+.details a{
+	text-decoration: underline;
+	letter-spacing: 2px;
+	color: #ffffe3;
+}
 </style>
