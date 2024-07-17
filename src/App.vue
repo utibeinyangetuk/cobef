9bc5c638be574de844b922ad8c81b8a8cc790160
utibe inyangetuk <inyangutibz@gmail.com>
2024-07-17 11:19:57 +0100

    Css variables

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,15 @@ import CustomCursor from "./components/CustomCursor.vue";
 @import url('https://fonts.cdnfonts.com/css/satoshi');
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
+:root {
+	--primary-background: #000000;
+	--primary-text-color: #ffffe3;
+	--secondary-text-color: #535349;
+	--hover-color: rgb(27, 72, 72);
+	--solid-border: 1px solid #5353497a;
+	--dashed-border: 1px dashed #5353497a;
+}
+
 body,
 a,
 * {
@@ -37,15 +46,15 @@ a,
 	list-style: none;
 
 	::-webkit-scrollbar {
-		width: 0px;
+		width: 4px;
 	}
 
 	::-webkit-scrollbar-track {
-		background: #000000;
+		background: var(--primary-background);
 	}
 
 	::-webkit-scrollbar-thumb {
-		background: gray;
+		background: var(--primary-text-color);
 		border-radius: 10px;
 	}
 }
