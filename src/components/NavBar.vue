b45d6633b72ad3ff73347c3fdefeef2f3efa5cfd
utibe inyangetuk <inyangutibz@gmail.com>
2024-07-21 21:01:34 +0100

    Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import NavMenu from './NavMenu.vue';
+import NavMenu from "./NavMenu.vue";
 </script>
 
 <template>
@@ -7,28 +7,36 @@ import NavMenu from './NavMenu.vue';
 		<div class="logo">
 			<h1>Cobef</h1>
 		</div>
+		<div class="nav-links">
+			<router-link to="/about">About</router-link>
+			<router-link to="/products">Products</router-link>
+			<router-link to="/project">Project</router-link>
+			<router-link to="/contact">Contact</router-link>
+		</div>
 		<NavMenu />
 	</div>
 </template>
 <style scoped>
 .page-container {
-	width: 100%;
-	position: fixed;
+	width: 93.8vw;
+	position: absolute;
 	top: 0;
-	left: 0;
+	left: 10;
 	height: 60px;
-	border-bottom: 1px solid #5353497a;
+	border: var(--pry-border);
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 	z-index: 999;
-	background-color: var(--primary-background)
+	background-color: var(--pry-background);
+	margin-top: 10px;
+	border-radius: 20px;
 }
 
 .logo {
 	display: flex;
 	justify-content: flex-start;
-	padding: 60px;
+	padding: 30px 0px 30px;
 }
 
 .logo h1 {
@@ -39,5 +47,23 @@ import NavMenu from './NavMenu.vue';
 	-webkit-text-stroke-width: 0.4px;
 	-webkit-text-stroke-color: #ffffff;
 	white-space: nowrap;
+	margin-left: 40px;
+}
+.nav-links {
+	display: flex;
+	justify-content: space-between;
+	width: 25%;
+	margin-right: 140px;
+}
+.nav-links a {
+	text-transform: uppercase;
+	font-size: 13px;
+	letter-spacing: 1px;
+	transition: 0.6s ease-in-out;
+	color: var(--sec-text);
+}
+.nav-links a:hover {
+	transition: 0.6s ease-in-out;
+	color: var(--link-hover);
 }
 </style>
