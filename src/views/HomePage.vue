f9437ce52c0a2acab79faace621a7841710ebd6f
utibe inyangetuk <inyangutibz@gmail.com>
2024-07-21 21:01:13 +0100

    Homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,127 +1,402 @@
 <script setup>
-import CurrentPage from '@/components/CurrentPage.vue';
-import Footer from '@/components/Footer.vue';
-import ImageSlot from '@/slots/Image-slot.vue';
-import { useRouter } from 'vue-router';
-const router = useRouter();
-const navigateTo = (path) => router.push(path);
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-	<CurrentPage />
-	<div class="main-wrapper">
-		<ImageSlot class="container1">
-			<template v-slot:title>
-				<h2>Our workforce</h2>
-			</template>
-			<template v-slot:button2>
-				<button @click="navigateTo('/#')" class="button">Take a tour</button>
-			</template>
-		</ImageSlot>
-		<ImageSlot class="container2">
-			<template v-slot:title>
-				<h2>About us</h2>
-			</template>
-			<template v-slot:button2>
-				<button @click="navigateTo('/#')" class="button">Learn more</button>
-			</template>
-		</ImageSlot>
-		<ImageSlot class="container3">
-			<template v-slot:title>
-				<h2>Products/Services</h2>
-			</template>
-			<template v-slot:button2>
-				<button @click="navigateTo('/#')" class="button">View Catalog</button>
-			</template>
-		</ImageSlot>
-		<ImageSlot class="container4">
-			<template v-slot:title>
-				<h2>We are hiring</h2>
-			</template>
-			<template v-slot:button2>
-				<button @click="navigateTo('/#')" class="button">Apply Now</button>
-			</template>
-		</ImageSlot>
+	<div class="main-container">
+		<div class="main-wrapper">
+			<div class="header">
+				<p>
+					Great products elevate your business. Lorem ipsum, dolor sit amet
+					consectetur adipisicing elit. Voluptatem quasi ullam aperiam
+					veniam id sunt, ipsa, nemo enim illum quo placeat eaque ipsam
+					deleniti, recusandae unde perferendis dolorem nostrum architecto!
+				</p>
+				<div class="video-wrapper">
+					<video autoplay muted loop>
+						<source src="../assets/videos/video.mp4" type="video/mp4" />
+						Sorry, your browser does not support this video tag.
+					</video>
+				</div>
+			</div>
+			<div class="about-us-wrapper">
+				<div class="large-header">
+					<h1>
+						<span>.Beyond visions.</span> <br />
+						.within reach.
+					</h1>
+				</div>
+				<div class="content">
+					<div class="company-video">
+						<video autoplay muted loop>
+							<source src="../assets/videos/video.mp4" type="video/mp4" />
+							Sorry, your browser does not support this video tag.
+						</video>
+					</div>
+					<div class="vision-text">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Sunt ducimus blanditiis repudiandae adipisci aspernatur
+							laborum corporis unde explicabo commodi officiis, magnam
+							quaerat repudiandae adipisci aspernatur laborum corporis
+							unde explicabo commodi officiis, magnam quaerat dolor
+							saepe, veniam voluptatum maxime. Itaque, dolorem numquam.
+							<br /><br />
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Sunt ducimus blanditiis repudiandae adipisci aspernatur
+							laborum corporis unde explicabo commodi officiis, magnam
+							quaerat repudiandae adipisci aspernatur laborum corporis
+							unde explicabo commodi officiis, magnam quaerat dolor
+							saepe, veniam voluptatum maxime. Itaque, dolorem numquam.
+						</p>
+						<router-link to="/about">
+							About us <i class="uil uil-arrow-right" />
+						</router-link>
+					</div>
+				</div>
+			</div>
+			<div class="featured-products">
+				<div class="product-heading">
+					<h1>Featured Products</h1>
+					<div class="description">
+						<p>
+							A selection of someof our best produts Lorem ipsum dolor sit
+							amet, consectetur adipisicing elit. Libero, doloremque iste!
+							Illum aperiam, maiores eaque eligendi velit aut, provident
+							nesciunt tempora accusantium ducimus qui beatae perferendis!
+						</p>
+						<router-link to="/products">View all products<i class="uil uil-arrow-right" /></router-link>
+					</div>
+				</div>
+				<div class="product-container">
+					<div class="product-card">
+						<div class="image-wrapper">
+							<img src="../assets/materials/hdpe.jpg" />
+						</div>
+						<h1>Hdpe pipes</h1>
+					</div>
+					<div class="product-card">
+						<div class="image-wrapper">
+							<img src="../assets/materials/Tank.jpeg" />
+						</div>
+						<h1>Tanks</h1>
+					</div>
+					<div class="product-card">
+						<div class="image-wrapper">
+							<img src="../assets/materials/pressure.jpg" />
+						</div>
+						<h1>Pressure pipes</h1>
+					</div>
+					<div class="product-card">
+						<div class="image-wrapper">
+							<img src="../assets/materials/LAPETA.jpg" />
+						</div>
+						<h1>Lapeta</h1>
+					</div>
+				</div>
+				<div class="vacancy-container">
+					<h1>Want to work with us?</h1>
+					<div class="vacancy">
+						<div class="hiring-image-wrapper">
+							<img src="../assets/homepage-images/hiring.jpg" />
+						</div>
+						<div class="hiring-text-wrapper">
+							<span class=""></span>
+							<h2>We are hiring</h2>
+							<ul>
+								<li>Nysc Corpers</li>
+								<li>Student Interns</li>
+								<li>Full-time workers</li>
+								<li>Contract Workers</li>
+							</ul>
+							<div class="button-wrapper">
+								<router-link to="/contact">Contact us<i class="uil uil-arrow-right" /></router-link>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
 	</div>
 	<Footer />
 </template>
 <style scoped>
-.container4 {
-	background-image: url(../assets/homepage-images/hiring.jpg);
-	background-position: center;
+.main-container {
+	margin-top: 30px;
+	background: var(--ter-background);
+	background-size: var(--ter-background-size);
+	background-color: var(--pry-background);
 }
 
-.container2 {
-	background-image: url(../assets/homepage-images/aboutus.avif);
-	background-position: center;
+.main-wrapper {
+	border: var(--pry-border);
+	border-top: 0;
+	border-left: 0;
+	border-right: 0;
 }
 
-.container3 {
-	background-image: url(../assets/homepage-images/bluepipes.jpg);
-	background-position: center;
+.header {
+	padding: var(--pry-padding);
 }
 
-.container1 {
-	background-image: url(../assets/homepage-images/company.jpg);
+.header p {
+	width: 50%;
+	line-height: 1.5em;
+	margin-bottom: 20px;
+	color: var(--sec-text);
 }
 
-.container1,
-.container2,
-.container3,
-.container4 {
-	background-repeat: no-repeat;
-	background-size: cover;
-	height: 130vh;
-	position: relative;
+.video-wrapper video {
+	border-radius: 20px;
+	width: 100%;
+	box-shadow: var(--box-shadow);
 }
 
-h2 {
+.about-us-wrapper {
+	background-color: var(--sec-background);
+}
+
+.large-header h1 {
+	font-size: 100px;
+	text-transform: uppercase;
 	letter-spacing: 2px;
-	font-size: 8vw;
+	text-shadow: var(--text-shadow);
+	line-height: 1.2em;
+	color: var(--ter-text);
+	margin-left: 20px;
+}
+
+.large-header span {
+	margin-left: 240px;
+}
+
+.content {
+	display: flex;
+	justify-content: space-between;
+	padding: 20px;
+	gap: 40px;
+}
+
+.company-video {
+	width: 200vw;
+	margin-top: 10%;
+	box-shadow: var(--box-shadow);
+	border-radius: 0.4em;
+}
+
+.company-video video {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+}
+
+.vision-text {
+	margin-left: 20px;
+}
+
+.vision-text p {
+	font-size: 15px;
+	font-weight: 400;
+	line-height: 1.5em;
+	letter-spacing: 1px;
+	margin-bottom: 20px;
+	padding: 10px;
+	color: #001317;
+}
+
+.vision-text a {
+	background-color: transparent;
 	text-transform: capitalize;
-	margin-top: 10px;
-	color: #ffffff;
-	text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+	font-weight: bold;
+	padding: 13px 20px;
+	transition: 1s ease-in-out;
+	border-radius: 0.4em;
+	margin-left: 10px;
+	border: 1px solid #000000;
+	font-size: 14px;
+	color: var(--ter-text);
+	letter-spacing: 1px;
 }
 
-.button {
-	background-color: white;
-	border: none;
-	border-radius: 5px;
-	padding: 15px 30px;
-	font-size: 30px;
+.vision-text a:hover {
+	transition: 1s ease-in-out;
+	background-color: #000000;
+	color: #ffffff;
+}
+
+.product-heading {
+	display: flex;
+	align-items: center;
+	padding: 10px;
+	margin-top: 15px;
+}
+
+.product-heading h1 {
+	width: 100%;
+	font-size: 80px;
+	color: var(--pry-text);
+}
+
+.product-heading .description {
+	width: 40%;
+	height: 160px;
+}
+
+.description p {
+	margin-bottom: 20px;
+	color: var(--sec-text);
+	font-size: 14px;
+	line-height: 1.5em;
+}
+
+.description a {
+	background-color: transparent;
+	text-transform: capitalize;
 	font-weight: bold;
-	cursor: pointer;
-	transition: transform 0.4s ease, box-shadow 0.5s ease;
-	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+	padding: 13px;
+	transition: 1s ease-in-out;
+	border-radius: 0.4em;
+	border: 1px solid #ffffe3a9;
+	font-size: 14px;
+	color: var(--pry-text);
 	letter-spacing: 2px;
+}
+
+.description a:hover {
+	background: var(--sec-background);
+	color: #000000;
+}
+
+.product-container {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	gap: 20px;
+	padding: 10px;
+	margin-top: 20px;
+}
+
+.product-card {
+	height: 500px;
+	width: 600px;
+}
+
+.product-card .image-wrapper {
+	border-radius: 20px;
+	height: 400px;
+}
+
+.product-card .image-wrapper img {
+	width: 100%;
+	object-fit: cover;
+}
+
+.product-card h1 {
+	margin-left: 20px;
+	margin-top: 5px;
+	text-transform: uppercase;
+	font-size: 40px;
+	color: var(--pry-text);
+}
+
+.vacancy-container {
+	margin-top: 40px;
+	border: var(--pry-border);
+	border-right: none;
+	border-left: none;
+	background: var(--checkered-background);
+	background-size: var(--checkered-background-size);
+	background-repeat: repeat;
+}
+
+.vacancy-container h1 {
+	font-size: 50px;
+	color: #ffffe3;
+	text-transform: uppercase;
+	margin-left: 40px;
+	letter-spacing: 2px;
+}
+
+.vacancy {
+	padding: 50px;
+	display: flex;
+	position: relative;
+	align-items: center;
+}
+
+.hiring-image-wrapper {
+	height: 400px;
+	width: 700px;
+	border-radius: 10px;
+	margin-left: 100px;
+}
+
+.hiring-image-wrapper img {
+	width: 100%;
+}
+
+.hiring-text-wrapper {
+	height: fit-content;
+	width: 500px;
+	background-color: #B9C089;
+	border: var(--solid-border);
+	position: absolute;
+	left: 750px;
+	border-radius: 10px;
+	padding: 20px;
+	box-shadow: var(--box-shadow);
+}
+
+.hiring-text-wrapper h2 {
+	color: #001317;
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	font-weight: 700;
+	font-family: poppins;
+	font-size: 2.25rem;
+	margin-left: 8px;
+}
+
+.hiring-text-wrapper ul {
+	padding: 10px;
+}
+
+.hiring-text-wrapper ul li {
+	color: #001317;
+	font-size: 17px;
+	font-weight: 700;
+	letter-spacing: 1.5px;
+	line-height: 1.8em;
 	text-transform: uppercase;
 }
 
-.button:hover {
-	transform: translateY(-5px);
-	box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
+.button-wrapper {
+	margin-top: 10px;
+	padding: 13px;
+	border: 1px solid #001317;
+	width: fit-content;
+	border-radius: 5px;
+	display: flex;
+	align-items: center;
+	margin-left: 10px;
+	transition: 0.6s ease-in-out;
 }
 
-@media screen and (max-width: 600px) {
-	.button {
-		font-size: 15px;
-		padding: 14px;
-	}
+.button-wrapper a {
+	color: #001317;
+	font-weight: 700;
+	letter-spacing: 2px;
+	font-size: 0.7em;
+	text-transform: uppercase;
+}
 
-	.container1,
-	.container2,
-	.container3,
-	.container4 {
-		height: 60vh;
-	}
+.button-wrapper a:hover {
+	color: #ffffff;
+}
 
-	.container4 {
-		width: 100%;
-	}
-
-	h2 {
-		margin-top: 100px;
-	}
+.button-wrapper:hover {
+	background-color: #000000;
+	transition: 0.6s ease-in-out;
 }
 </style>
