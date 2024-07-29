a679d7083b2ca8f822c42030b9bf042b113492ab
utibe inyangetuk <inyangutibz@gmail.com>
2024-07-29 15:10:45 +0100

    products page

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,18 +1,15 @@
-<script setup>
-import Footer from '@/components/Footer.vue';
-</script>
 <template>
 	<div class="container">
 		<div class="header">
-			<p>Our Products</p>
-			<h2>
+			<h1>Our Products</h1>
+			<p>
 				Cobef International Ltd retains quality delivery and clients
 				satisfaction as its driving force. Based on ISO certification, we
 				are guided by the highest safety and quality standards to ensure
 				that our clients receive the highest levels of satisfaction in
 				addition to the commercial experience necessary to address
 				technically challenging projects.
-			</h2>
+			</p>
 		</div>
 		<div class="catalog">
 			<p class="secondary-header">Our Catalog</p>
@@ -137,108 +134,93 @@ import Footer from '@/components/Footer.vue';
 			</div>
 		</div>
 	</div>
-	<Footer />
 </template>
 <style scoped>
 .container {
-	background-color: var(--pry-background);
-	background: var(--ter-background);
-	background-size: var(--ter-background-size);
 	margin-top: 30px;
-	padding: 0 40px;
 }
 
 .header {
 	width: 100%;
-	color: #fafafa;
-	margin-bottom: 70px;
+	color: var(--ter-text-color);
+	margin-top: 20px;
+	border-bottom: var(--pry-border);
+}
+
+.header h1 {
+	text-transform: uppercase;
+	letter-spacing: 1px;
+	font-size: var(--pry-font-size);
+	font-weight: bold;
+	color: var(--pry-text-color);
+	align-content: center;
+	height: fit-content;
+	margin-top: 0.4em;
+	margin-bottom: 0.3em;
+	text-align: center;
 }
 
 .header p {
-	text-transform: capitalize;
-	letter-spacing: 2px;
-	font-size: 50px;
-	margin-top: 0px;
-	margin-bottom: 30px;
-	font-weight: bold;
-	color: var(--pry-text);
-	align-content: center;
-	height: 100px;
-}
-
-.header h2 {
-	font-weight: 400;
-	font-size: 34px;
-	letter-spacing: 1px;
+	font-weight: 100;
+	font-family: "satoshi", sans-serif;
+	font-size: 1.25rem;
 	line-height: 1.6em;
+	letter-spacing: 2px;
+	padding: 0 40px;
+	margin-bottom: 1rem;
 }
 
 .secondary-header {
-	text-transform: capitalize;
+	text-transform: uppercase;
 	letter-spacing: 2px;
-	font-size: 50px;
+	font-size: var(--pry-font-size);
 	margin-top: 0px;
-	margin-bottom: 30px;
 	font-weight: bold;
-	color: #ffffe3;
-	align-content: center;
+	color: var(--pry-text-color);
+	text-align: center;
 	height: 100px;
+	border-bottom: var(--pry-border);
+	align-content: center;
 }
 
 .product-container .product {
 	display: flex;
 	gap: 30px;
-	margin-top: 70px;
 	align-items: center;
-	justify-content: center;
+	border: var(--pry-border);
+	border-top: 0;
+	padding: 10px;
 }
 
 .product-container .product img {
 	width: 100%;
-	height: 80vh;
-	margin-bottom: 70px;
+	height: 50vh;
 	border-radius: 10px;
+	border: var(--pry-border);
+	object-fit: fill;
 }
 
 .product-container .product-description h1 {
 	letter-spacing: 2px;
-	font-size: 70px;
-	color: #ffffe3;
+	font-size: var(--pry-font-size);
+	color: var(--pry-text-color);
 }
 
 .product-container .product-description p {
 	width: 80%;
-	line-height: 50px;
-	font-weight: normal;
-	color: #fafafa;
-	margin-right: -780px;
-	text-transform: uppercase;
+	line-height: 2rem;
+	font-weight: 300;
+	color: var(--pry-text-color);
 	letter-spacing: 2px;
-	font-size: 14px;
-	margin-top: 40px;
-	margin-bottom: 40px;
+	font-size: 1em;
+	margin-top: 1em;
+	margin-right: -40rem;
 }
 
-@media only screen and (min-width:601px) and (max-width:1024px) {
-	.container {
-		padding: 0 10px;
-	}
-
-	.header {
-		margin-bottom: 50px;
-	}
-
+@media only screen and (min-width: 601px) and (max-width: 1024px) {
 	.header p {
-		margin-bottom: -.5em;
-	}
-
-	.header h2 {
-		font-size: 1.3em;
-		line-height: 2em;
-	}
-
-	.secondary-header {
-		margin-bottom: -1.2em;
+		line-height: 1.7em;
+		text-align: center;
 	}
 
 	.product {
@@ -252,9 +234,9 @@ import Footer from '@/components/Footer.vue';
 	}
 
 	.product-container .product img {
-		height: 60vh;
+		height: 50vh;
 		background-size: cover;
-		object-fit: cover;
+		object-fit: fill;
 		margin-bottom: -1em;
 	}
 
@@ -263,65 +245,56 @@ import Footer from '@/components/Footer.vue';
 	}
 
 	.product-container .product-description p {
-		font-size: .8em;
+		font-size: 1em;
 		margin-top: 0em;
-		line-height: 3em;
+		line-height: 2em;
 		width: 100%;
+		font-weight: 300;
 	}
 }
 
 @media only screen and (max-width: 600px) {
 	.container {
-		padding: 0 10px;
+		margin-top: 55px;
 	}
-
 	.header {
-		margin-bottom: 25px;
+		margin-top: 0.1rem;
 	}
 
 	.header p {
-		font-size: 2.4em;
-		margin-bottom: -.9em;
-	}
-
-	.header h2 {
+		font-weight: 400;
 		font-size: 1em;
-		line-height: 2em;
-	}
-
-	.secondary-header {
-		margin-bottom: -1.2em;
-		font-size: 2.8em;
-		width: 100%;
-		height: fit-content;
+		padding: 0.4em;
+		text-align: center;
 	}
 
 	.product {
 		display: flex;
 		flex-direction: column;
 	}
-
 	.product:nth-child(even) {
 		display: flex;
 		flex-direction: column-reverse;
 	}
 
 	.product-container .product img {
-		height: 60vh;
+		height: 40vh;
 		background-size: cover;
-		object-fit: cover;
-		margin-bottom: -1em;
+		object-fit: fill;
 	}
 
 	.product-container .product-description h1 {
-		font-size: 1.7em;
+		font-size: 1.4em;
+		margin-bottom: 0.2em;
 	}
 
 	.product-container .product-description p {
-		font-size: .6em;
-		margin-top: 0em;
-		line-height: 3em;
+		font-size: 1em;
+		line-height: 2em;
 		width: 100%;
+		margin-top: 0px;
+		letter-spacing: 1px;
+		font-weight: 400;
 	}
 }
 </style>
