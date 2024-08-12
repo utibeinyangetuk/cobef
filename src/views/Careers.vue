7e602a938661a3e1bf2043f1392330766a9ecf6f
utibe inyangetuk <inyangutibz@gmail.com>
2024-08-12 10:26:40 +0100

    careers page

---
+++ src/views/Careers.vue
@@ -0,0 +1,392 @@
+<template>
+	<div class="container">
+		<header>
+			<h1>Careers</h1>
+			<p>
+				Be part of an innovative and dynamic company. We are always looking
+				for talented individuals to join our team and help us shape the
+				future.
+			</p>
+		</header>
+		<div class="write-up-container">
+			<div class="heading-wrapper">
+				<h2>our culture</h2>
+				<h1>why <span style="color: var(--hover);font-weight: bold;">COBEF?</span></h1>
+				<p>
+					Cobef is a great place to work for many reasons. It starts with a
+					mission that drives everything we do and shapes how we do it.
+				</p>
+			</div>
+			<div class="list-wrapper">
+				<ul>
+					<li>
+						<h3>
+							lorem ipsum dolor sit emet asperiores voluptate suscipit
+							est provident amet repellat
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Rem ad tempora veniam, asperiores voluptate suscipit est
+							provident amet repellat magni! Perspiciatis eligendi nemo
+							quisquam ea nulla vel aliquam cum animi!
+						</p>
+					</li>
+					<li>
+						<h3>
+							lorem ipsum dolor sit emet asperiores voluptate suscipit
+							est provident amet repellat
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Rem ad tempora veniam, asperiores voluptate suscipit est
+							provident amet repellat magni! Perspiciatis eligendi nemo
+							quisquam ea nulla vel aliquam cum animi!
+						</p>
+					</li>
+					<li>
+						<h3>
+							lorem ipsum dolor sit emet asperiores voluptate suscipit
+							est provident amet repellat
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Rem ad tempora veniam, asperiores voluptate suscipit est
+							provident amet repellat magni! Perspiciatis eligendi nemo
+							quisquam ea nulla vel aliquam cum animi!
+						</p>
+					</li>
+					<li>
+						<h3>
+							lorem ipsum dolor sit emet asperiores voluptate suscipit
+							est provident amet repellat
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Rem ad tempora veniam, asperiores voluptate suscipit est
+							provident amet repellat magni! Perspiciatis eligendi nemo
+							quisquam ea nulla vel aliquam cum animi!
+						</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="open-positions">
+			<h1>Open Positions</h1>
+			<div class="position-wrapper">
+				<section>
+					<h1>Cobef Internships</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Autem dicta numquam deserunt dignissimos repudiandae voluptas
+						iste molestiae fuga! Dicta voluptatem consectetur et nobis
+						totam quia mollitia, architecto delectus facere dolore.
+					</p>
+					<ul>
+						<li>Transport manager</li>
+						<li>Legal head</li>
+						<li>Machine operator</li>
+						<li>Canteen</li>
+					</ul>
+					<div class="link-wrapper">
+						<router-link to="/contact">Apply Now</router-link>
+					</div>
+				</section>
+				<section>
+					<h1>NYSC Corpers</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Autem dicta numquam deserunt dignissimos repudiandae voluptas
+						iste molestiae fuga! Dicta voluptatem consectetur et nobis
+						totam quia mollitia, architecto delectus facere dolore.
+					</p>
+					<ul>
+						<li>Transport manager</li>
+						<li>Legal head</li>
+						<li>Machine operator</li>
+						<li>Canteen</li>
+					</ul>
+					<div class="link-wrapper">
+						<router-link to="/contact">Apply Now</router-link>
+					</div>
+				</section>
+				<section>
+					<h1>Cobef Internships</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Autem dicta numquam deserunt dignissimos repudiandae voluptas
+						iste molestiae fuga! Dicta voluptatem consectetur et nobis
+						totam quia mollitia, architecto delectus facere dolore.
+					</p>
+					<ul>
+						<li>Transport manager</li>
+						<li>Legal head</li>
+						<li>Machine operator</li>
+						<li>Canteen</li>
+					</ul>
+					<div class="link-wrapper">
+						<router-link to="/contact">Apply Now</router-link>
+					</div>
+				</section>
+				<section>
+					<h1>Cobef Internships</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Autem dicta numquam deserunt dignissimos repudiandae voluptas
+						iste molestiae fuga! Dicta voluptatem consectetur et nobis
+						totam quia mollitia, architecto delectus facere dolore.
+					</p>
+					<ul>
+						<li>Transport manager</li>
+						<li>Legal head</li>
+						<li>Machine operator</li>
+						<li>Canteen</li>
+					</ul>
+					<div class="link-wrapper">
+						<router-link to="/contact">Apply Now</router-link>
+					</div>
+				</section>
+				<section>
+					<h1>Cobef Internships</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Autem dicta numquam deserunt dignissimos repudiandae voluptas
+						iste molestiae fuga! Dicta voluptatem consectetur et nobis
+						totam quia mollitia, architecto delectus facere dolore.
+					</p>
+					<ul>
+						<li>Transport manager</li>
+						<li>Legal head</li>
+						<li>Machine operator</li>
+						<li>Canteen</li>
+					</ul>
+					<div class="link-wrapper">
+						<router-link to="/contact">Apply Now</router-link>
+					</div>
+				</section>
+				<section>
+					<h1>Cobef Internships</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Autem dicta numquam deserunt dignissimos repudiandae voluptas
+						iste molestiae fuga! Dicta voluptatem consectetur et nobis
+						totam quia mollitia, architecto delectus facere dolore.
+					</p>
+					<ul>
+						<li>Transport manager</li>
+						<li>Legal head</li>
+						<li>Machine operator</li>
+						<li>Canteen</li>
+					</ul>
+					<div class="link-wrapper">
+						<router-link to="/contact">Apply Now</router-link>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+</template>
+<style scoped>
+	.container {
+		margin-top: 50px;
+	}
+
+	header {
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	header h1 {
+		color: var(--pry-text-color);
+		letter-spacing: 4px;
+		text-transform: uppercase;
+		font-size: 2em;
+		margin-bottom: 10px;
+	}
+
+	header p {
+		color: var(--sec-text-color);
+		font-size: 1.2em;
+		width: 60%;
+		line-height: 1.4em;
+		letter-spacing: 2px;
+	}
+
+	.write-up-container {
+		display: flex;
+		padding: 10px;
+	}
+
+	.heading-wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		width: 48%;
+	}
+
+	.write-up-container .heading-wrapper h2 {
+		font-size: 1.2em;
+		letter-spacing: 2px;
+		color: var(--pry-text-color);
+		margin-bottom: 7px;
+	}
+
+	.write-up-container .heading-wrapper h1 {
+		color: var(--pry-text-color);
+		letter-spacing: 2px;
+		font-size: 3.2em;
+		font-weight: 100;
+		margin-bottom: 6px;
+	}
+
+	.write-up-container .heading-wrapper p {
+		color: var(--sec-text-color);
+		line-height: 1.8em;
+		width: 100%;
+		font-size: 1.2em;
+		margin-bottom: 10px;
+	}
+
+	.write-up-container .list-wrapper ul {
+		margin-top: 60px;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		gap: 10px;
+		padding: 10px;
+	}
+
+	.list-wrapper li {
+		margin-bottom: 20px;
+		list-style: decimal;
+		padding: 6px;
+	}
+
+	.list-wrapper li h3 {
+		color: var(--pry-text-color);
+		font-size: 1em;
+		line-height: 1.5em;
+		letter-spacing: 1px;
+		margin-bottom: 10px;
+	}
+
+	.list-wrapper li p {
+		color: var(--sec-text-color);
+		font-size: 0.9em;
+		line-height: 2em;
+	}
+
+	.list-wrapper li::marker {
+		color: var(--highlight);
+	}
+
+	.open-positions h1 {
+		text-align: center;
+		font-size: 3em;
+		color: var(--pry-text-color);
+		letter-spacing: 2px;
+		margin-bottom: 20px;
+	}
+
+	.position-wrapper {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+		padding: 10px;
+		gap: 10px;
+		width: 100%;
+	}
+
+	.position-wrapper > section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		border: var(--pry-border);
+		border-radius: 5px;
+		padding: 10px;
+		width: 100%;
+	}
+
+	.position-wrapper > section h1 {
+		text-align: center;
+		font-size: 2em;
+		letter-spacing: 2px;
+	}
+
+	.position-wrapper > section p {
+		color: var(--sec-text-color);
+		text-align: left;
+		line-height: 1.8em;
+		margin-bottom: 10px;
+	}
+
+	.position-wrapper > section ul {
+		margin-left: 20px;
+	}
+
+	.position-wrapper > section ul li {
+		margin-bottom: 7px;
+		color: var(--pry-text-color);
+		letter-spacing: 1px;
+	}
+
+	.position-wrapper > section ul li::marker {
+		color: var(--highlight);
+	}
+
+	.position-wrapper > section .link-wrapper {
+		margin: 10px 10px;
+	}
+
+	.position-wrapper > section .link-wrapper a {
+		background-color: var(--pry-btn);
+		border-radius: 5px;
+		padding: 8px;
+		letter-spacing: 2px;
+		color: var(--ter-text-color);
+		font-weight: bold;
+		font-size: 0.8em;
+	}
+
+	.position-wrapper > section .link-wrapper a:hover {
+		background-color: var(--hover);
+		color: var(--pry-text-color);
+	}
+
+	@media only screen and (max-width:867px) {
+		header p {
+			width: 100%;
+		}
+
+		.write-up-container {
+			display: flex;
+			flex-wrap: wrap;
+			flex-grow: 1;
+		}
+
+		.heading-wrapper {
+			margin-top: 20px;
+			width: 100%;
+			text-align: center;
+		}
+
+		.heading-wrapper h1 {
+			margin-top: 10px;
+		}
+
+		.list-wrapper {
+			margin-top: -60px;
+		}
+
+		.list-wrapper ul {
+			padding: 0px;
+		}
+
+		.list-wrapper li {
+			border: var(--pry-border);
+			border-radius: 5px;
+			padding: 0 10px;
+			width: 100%;
+			list-style: none;
+		}
+	}
+</style>
