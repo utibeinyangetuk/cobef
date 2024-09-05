95f8062d814c0c0e20a62f9640daeb86a48263d4
utibe inyangetuk <inyangutibz@gmail.com>
2024-09-05 12:22:25 +0100

    contact page redesign

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,201 +1,159 @@
 <template>
-	<div class="main-container">
-		<div class="title">
-			<h1>Contact us</h1>
-		</div>
-		<div class="company-info">
-			<div class="write-up">
-				<h1>Our inbox is open</h1>
-				<p>
-					The best part of our job is being able to please our customers
-					and neighbors. Whether you need help starting a new project, have
-					a question about our products and services, or need us to come
-					back for a repair, get in touch with our form.
-				</p>
-			</div>
-			<div class="form">
-				<div class="image-wrapper">
-					<img src="../assets/homepage-images/team.jpg" />
-				</div>
-				<div class="company-details">
-					<div class="location">
-						<h1>Our Office</h1>
-						<p>
-							Km 17 Port Harcourt/Aba Express Way P. O. Box, 2769 Port
-							Harcourt
-						</p>
-						<div class="button">
-							<a
-								href="https://www.google.com/maps/place/Cobef+International+Limited/@4.8610877,7.0891647,17.42z/data=!4m9!1m2!2m1!1sKm+17+Port+Harcourt%2FAba+Express+Way+P.+O.+Box,+2769+Port+Harcourt!3m5!1s0x1069d2ad21ae86bd:0x46fb3e4446572965!8m2!3d4.860476!4d7.0880525!16s%2Fg%2F11hzhc6m0m?entry=ttu"
-								target="_blank"
-								>View in maps</a
-							>
-						</div>
-					</div>
-					<div class="Phone">
-						<h1>Phone</h1>
-						<div class="number-wrapper">
-							<a href="tel:+1234567890">+(234) 567-890-9123</a>
-							<a href="tel:+1234567890">+(234) 567-890-9123</a>
-							<a href="tel:+1234567890">+(234) 567-890-9123</a>
-						</div>
-					</div>
-					<div class="email">
-						<h1>Email</h1>
-						<a href="mailto:info@cobefinternationalltd.com"
-							>info@cobefinternationalltd.com</a
-						>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="section1"></div>
+      <div class="section2">
+         <h1>Get in touch.</h1>
+         <div class="find-us">
+            <h2>Find us</h2>
+            <p>Km 17 Port Harcourt/Aba Express Way, P. O. Box 2769, Port Harcourt</p>
+            <i class="uil uil-location-point" /> <a href="https://www.google.com/maps/place/Cobef+International+Limited"
+               target="_blank" class="button">
+               View in Maps
+            </a>
+         </div>
+         <div class="enquiry">
+            <h2>Have an enquiry?</h2>
+            <p>Whether you have a question about a future project,want to know more about how
+               we work or are interested in a career with us,you can email us or give us a call by
+               using the links below.
+            </p>
+         </div>
+         <div class="links">
+            <div class="telephone">
+               <h2>Phone</h2>
+               <i class="uil uil-phone" /><a href="tel:+2345678909123">+(234) 567-890-9123</a><br>
+               <i class="uil uil-phone" /><a href="tel:+2345678909123">+(234) 567-890-9123</a><br>
+               <i class="uil uil-phone" /><a href="tel:+2345678909123">+(234) 567-890-9123</a><br>
+            </div>
+            <div class="email">
+               <h2>Email</h2>
+               <i class="uil uil-envelope" />
+               <a href="mailto:info@cobefinternationalltd.com">info@cobefinternationalltd.com</a><br>
+               <i class="uil uil-envelope" />
+               <a href="mailto:info@cobefinternationalltd.com">info@cobefinternationalltd.com</a><br>
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
 <style scoped>
-.main-container {
-	margin-top: 50px;
-}
-.title {
-	text-transform: uppercase;
-	padding: 20px;
-}
-.title h1 {
-	font-size: 4em;
-	text-shadow: 2px 6px 3px rgba(0, 0, 0, 0.7);
-	text-align: center;
-	letter-spacing: 3px;
-	font-family: poppins;
-	width: 100%;
-}
-.company-info {
-	width: 100%;
-	background: linear-gradient(to top, #fdf5e5, #ffffe3);
-	border-top-right-radius: 20%;
-	border-top-left-radius: 20%;
-}
-.write-up {
-	display: flex;
-	flex-direction: column;
-	text-align: center;
-	align-items: center;
-}
-.write-up h1 {
-	width: 80%;
-	text-transform: uppercase;
-	letter-spacing: 2px;
-	margin-top: 20px;
-	font-size: 1.7em;
-}
-.write-up p {
-	font-size: 1.6em;
-	width: 70%;
-	line-height: 2em;
-	margin-top: 10px;
-	font-weight: 200;
-}
-.form {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
-.image-wrapper {
-	width: 90%;
-}
-.image-wrapper img {
-	width: 100%;
-	height: 500px;
-	border-radius: 1em;
-	object-fit: cover;
-}
+   .container {
+      margin-top: 50px;
+      display: flex;
+   }
 
-.company-details {
-	display: flex;
-	justify-content: space-evenly;
-	width: 100%;
-	height: 100%;
-}
-.company-details h1 {
-	font-size: 0.9em;
-	text-transform: uppercase;
-	letter-spacing: 2px;
-	margin-top: 20px;
-	margin-bottom: 10px;
-}
-.button {
-	padding: 10px 0;
-	margin-top: 6px;
-}
-.button a {
-	border: 1px solid;
-	padding: 6px;
-	border-radius: 3px;
-	color: #212121;
-}
-.button a:hover {
-	background-color: #212121;
-	color: #ffffe3;
-	border: none;
-}
-.email a,
-.number-wrapper a {
-	font-size: 0.8em;
-	letter-spacing: 1px;
-	color: #212121;
-}
-.email a:hover,
-.number-wrapper a:hover {
-	color: var(--link-hover);
-}
-.number-wrapper {
-	display: flex;
-	flex-direction: column;
-	line-height: 2em;
-}
-.location p {
-	font-size: 0.9em;
-}
+   .section1 {
+      width: 100%;
+      background: url('../assets/homepage-images/team.jpg');
+      background-repeat: no-repeat;
+      object-fit: cover;
+      background-size: cover;
+   }
 
-@media only screen and (max-width: 768px) {
-	.title h1 {
-		font-size: 3em;
-		text-shadow: 2px 5px 3px rgba(0, 0, 0, 0.7);
-		width: 100%;
-	}
-	.company-info {
-		border-top-right-radius: 10%;
-		border-top-left-radius: 10%;
-	}
-	.write-up h1 {
-		font-size: 1em;
-		width: 100%;
-		letter-spacing: 1px;
-		font-family: poppins;
-	}
-	.write-up p {
-		font-size: 1em;
-		width: 90%;
-	}
-	.image-wrapper img {
-		height: 250px;
-		object-fit: fill;
-	}
-	.company-details {
-		display: flex;
-		flex-direction: column-reverse;
-		padding: 0 20px;
-	}
-	.company-details h1 {
-		margin-bottom: 5px;
-	}
-	.location,
-	.Phone,
-	.email {
-		border-bottom: var(--pry-border);
-	}
-	.email a,
-	.number-wrapper,
-	.location a {
-		margin-bottom: 10px;
-	}
-}
+   .section2 {
+      height: 100vh;
+      width: 100%;
+      background-color: var(--pry-background);
+      padding: 40px;
+      display: flex;
+      flex-direction: column;
+   }
+
+   .section2 a {
+      color: var(--pry-text-color);
+      font-size: .8em;
+      letter-spacing: .7px;
+   }
+
+   .section2 a:hover {
+      color: var(--hover);
+   }
+
+   .section2 i {
+      display: inline-block;
+      width: 23px;
+      height: 23px;
+      border-radius: 50%;
+      border: 1px solid #000;
+      text-align: center;
+      font-size: 14px;
+      margin-bottom: .7em;
+      margin-right: 3px;
+   }
+
+   .section2 h1 {
+      color: var(--pry-text-color);
+      font-family: Planer;
+      letter-spacing: 2px;
+      font-size: 3.8em;
+      margin-bottom: 20px;
+      text-transform: uppercase;
+   }
+
+   .find-us {
+      margin-bottom: 2em;
+   }
+
+   .enquiry {
+      margin-bottom: 1.3em;
+   }
+
+   .find-us h2,
+   .enquiry h2 {
+      font-weight: 600;
+      font-size: .8em;
+      margin-bottom: 6px;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+   }
+
+   .find-us p,
+   .enquiry p {
+      font-size: .8em;
+      margin-bottom: 6px;
+      line-height: 2em;
+   }
+
+   .links {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+   }
+
+   .links h2 {
+      margin-bottom: 4px;
+      letter-spacing: 1px;
+      font-size: .8em;
+      font-weight: 600;
+      text-transform: uppercase;
+      margin-bottom: .5em;
+   }
+
+   .logo-wrapper img {
+      height: 80px;
+      width: 80px;
+   }
+
+   .footer-wrapper {
+      width: 35%;
+      display: flex;
+      justify-content: space-between;
+   }
+
+   @media only screen and (max-width:920px) {
+      .section2 h1 {
+         font-size: 2.5em;
+      }
+      .section2{
+         padding: 20px;
+      }
+
+      .container {
+         display: flex;
+         flex-direction: column;
+      }
+
+      .links h2 {
+         margin-top: 20px;
+      }
+   }
 </style>
