e3170615cb41d7f89bdfab0cbda0260c534ea573
utibe inyangetuk <inyangutibz@gmail.com>
2024-09-16 22:48:12 +0100

    Final Footer design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,6 +17,7 @@
             <div class="links">
                <a href="#">Linkedin</a>
                <a href="#">Whatsapp</a>
+               <a href="#">Facebook</a>
             </div>
          </div>
          <div class="connect">
@@ -37,18 +38,25 @@
 </template>
 <style scoped>
    footer {
-      background: var(--pry-background);
+      background: var(--background1);
+      border: var(--border);
+      width: 100%;
    }
 
    .container {
-      display: flex;
+      display: grid;
+      gap: 10px;
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      padding: 10px;
    }
 
-   .index,
-   .social {
-      height: 100%;
-      width: 60vw;
-      padding: 10px;
+   .container h1 {
+      font-size: 1em;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      background-color: var(--background2);
+      padding: 5px;
+      color: var(--sec-text);
    }
 
    .links {
@@ -56,131 +64,61 @@
       flex-direction: column;
    }
 
-   .index a,
-   .social a {
-      border: var(--sec-border);
-      transition: 1s ease-in-out;
+   .links a {
+      border: var(--border);
       margin-top: 10px;
-      border-radius: 3px;
-      font-size: 0.7em;
-      color: var(--pry-text-color);
-      padding: 10px;
-      letter-spacing: 1.5px;
-   }
-
-   .index a:hover,
-   .social a:hover {
-      background: var(--sec-background);
-      color: var(--hover);
-      transition: 1s ease-in-out;
-   }
-
-   .index h1,
-   .social h1 {
-      font-weight: 600;
-      letter-spacing: 2px;
-      font-size: 0.8em;
+      padding: 5px;
+      color: var(--pry-text);
+      font-size: .8em;
+      letter-spacing: .6px;
       text-transform: uppercase;
-      padding: 10px;
-      border-radius: 5px;
-      color: var(--ter-text-color);
-      background-color: var(--sec-background);
+   }
+
+   .links a:hover {
+      background-color: var(--background2);
+      color: var(--sec-text);
    }
 
    .connect {
-      margin: 8px;
-      min-width: 450px;
-      min-height: 30vh;
-      border-radius: 10px;
-      position: relative;
-      background-color: var(--sec-background);
-      background-size: cover;
-   }
-
-   .connect h1 {
-      font-weight: 600;
-      font-size: 1em;
-      letter-spacing: 3px;
-      line-height: 2rem;
-      text-transform: uppercase;
+      background-color: var(--background2);
       padding: 10px;
-      color: var(--ter-text-color);
-   }
-
-   .link-wrapper {
-      width: fit-content;
+      min-height: 40vh;
+      position: relative;
    }
 
    .link-wrapper a {
-      font-size: 0.7em;
-      background: var(--pry-background);
-      color: #000000;
-      padding: 30px 60px;
-      position: absolute;
-      letter-spacing: 2px;
-      font-weight: 600;
+      border: var(--sec-border);
+      padding: 10px;
+      color: var(--sec-text);
       text-transform: uppercase;
+      font-size: .8em;
+      letter-spacing: 1px;
+      position: absolute;
       bottom: 0;
       right: 0;
-      margin: 6px 6px 6px;
-      border-radius: 5px;
-      transition: 1s ease-in-out;
+      margin: 6px 6px;
    }
 
    .link-wrapper a:hover {
-      padding: 30px 100px;
-      transition: 1s ease-in-out;
-      background: var(--hover);
-      color: #ffffff;
+      background-color: var(--background1);
+      color: var(--pry-text);
    }
 
    .bottom-footer {
-      padding: 20px;
+      padding: 10px;
       display: flex;
       justify-content: space-between;
-      border-top: var(--sec-border);
+      border-top: var(--border);
    }
 
    .bottom-footer p {
-      font-size: 0.7em;
-      margin-right: 10em;
+      font-size: .7em;
    }
 
-   @media only screen and (max-width: 850px) {
-      .container {
-         display: flex;
-         flex-direction: column;
-      }
-
-      .index,
-      .social {
-         width: 100%;
-         padding: 3px;
-      }
-
-      .index h1,
-      .social h1 {
-         margin-top: 10px;
-         margin-bottom: 5px;
-      }
-
-      .connect {
-         min-width: 100%;
-         min-height: 30vh;
-         margin: 0;
-         border-radius: 2px;
-      }
-
+   @media only screen and (max-width:500px) {
       .bottom-footer {
          display: flex;
          flex-direction: column;
-         text-align: center;
-         padding: 0;
-      }
-
-      .bottom-footer p {
-         margin: 0;
-         padding:6px;
       }
    }
 </style>
