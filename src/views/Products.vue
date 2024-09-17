530eebdb07f598be52c70410e0cc3bf17ea92819
utibe inyangetuk <inyangutibz@gmail.com>
2024-09-17 20:14:39 +0100

    products page mock design

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,243 +1,148 @@
 <template>
    <div class="container">
-      <div class="header">
-         <h1>Products</h1>
-         <p>
-            Cobef International Ltd retains quality delivery and clients
-            satisfaction as its driving force. Based on ISO certification, we
-            are guided by the highest safety and quality standards to ensure
-            that our clients receive the highest levels of satisfaction in
-            addition to the commercial experience necessary to address
-            technically challenging projects.
-         </p>
-      </div>
-      <div class="catalog">
-         <div class="product-container">
-            <div class="product">
-               <img src="../assets/materials/hdpe.jpg" />
-               <div class="product-description">
-                  <h1>HDPE PIPES</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/LAPETA.jpg" />
-               <div class="product-description">
-                  <h1>LAPETA</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/Tank.jpeg" />
-               <div class="product-description">
-                  <h1>TANKS</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/pressure.jpg" />
-               <div class="product-description">
-                  <h1>PRESSURE PIPES</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/suction-hose.jpg" />
-               <div class="product-description">
-                  <h1>SUCTION HOSE</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/conduit.jpg" />
-               <div class="product-description">
-                  <h1>CONDUIT PIPES</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/corrugated-pipe.jpg" />
-               <div class="product-description">
-                  <h1>CORRUGATED PIPES</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/marine-rope.jpg" />
-               <div class="product-description">
-                  <h1>MARINE ROPES</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
-            <div class="product">
-               <img src="../assets/materials/pvc.jpg" />
-               <div class="product-description">
-                  <h1>PVC PIPES</h1>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Soluta, nesciunt sed vero sequi molestias quos quod
-                     expedita beatae perspiciatis, blanditiis ratione aliquid
-                     dignissimos dolores corrupti assumenda earum eum nihil.
-                     Est.
-                  </p>
-               </div>
-            </div>
+      <div class="left" ref="leftDiv">
+         <div v-for="(image, index) in images" :key="index" class="box" :style="{ backgroundImage: `url(${image})` }"
+            :data-index="index">
+            <!-- Image will be set via CSS background -->
          </div>
+      </div>
+      <div class="right">
+         <h1>{{ currentHeading }}</h1>
+         <p>{{ currentContent }}</p>
       </div>
    </div>
 </template>
 
+<script setup>
+   import { onMounted, ref } from 'vue';
+   import conduit from '../assets/materials/conduit.jpg';
+   import corrugated from '../assets/materials/corrugated-pipe.jpg';
+   import hdpe from '../assets/materials/hdpe.jpg';
+   import lapeta from '../assets/materials/LAPETA.jpg';
+   import marine_rope from '../assets/materials/marine-rope.jpg';
+   import pressure from '../assets/materials/pressure.jpg';
+   import pvc from '../assets/materials/pvc.jpg';
+   import suction from '../assets/materials/suction-hose.jpg';
+   import tank from '../assets/materials/Tank.jpeg';
+
+   // Create an array of the images
+   const images = [hdpe, lapeta, tank, pressure, suction, conduit, corrugated, marine_rope, pvc];
+
+   // Define headings and corresponding contents
+   const headings = [
+      'hdpe', 'lapeta', 'Plastic tanks',
+      'pressure pipes', 'suction hose', 'conduit pipes',
+      'corrugated pipes', 'marine rope', 'pvc pipes'
+   ];
+
+   const contents = [
+      '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.', '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.', '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.',
+      '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.', '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.', '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.',
+      '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.', '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.', '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, officia quaerat quis doloribus, natus nisi, rerum amet nobis vel ullam enim. In odit eligendi architecto repellendus et illo dolorum delectus.',
+   ];
+
+   const currentHeading = ref(headings[0]);
+   const currentContent = ref(contents[0]);
+
+   const updateRightDivContent = (entries) => {
+      entries.forEach(entry => {
+         if (entry.isIntersecting) {
+            const index = parseInt(entry.target.dataset.index);
+            currentHeading.value = headings[index];
+            currentContent.value = contents[index];
+         }
+      });
+   };
+
+   onMounted(() => {
+      const observer = new IntersectionObserver(updateRightDivContent, {
+         root: document.querySelector('.left'),
+         threshold: 0.5 // Trigger update when 50% of the box is visible
+      });
+
+      const boxes = document.querySelectorAll('.box');
+      boxes.forEach((box) => {
+         observer.observe(box);
+      });
+   });
+</script>
+
 <style scoped>
    .container {
-      margin-top: 50px;
-      background-color: var(--pry-background);
-   }
-
-   .header {
-      width: 100%;
-      margin-top: 20px;
-      text-align: center;
-   }
-
-   .header h1 {
-      text-transform: uppercase;
-      font-size: 5em;
-      font-weight: 700;
-      color: var(--pry-text-color);
-      height: fit-content;
-      letter-spacing: 2px;
-   }
-
-   .header p {
-      font-size: 1em;
-      line-height: 1.8em;
-      padding: 0 60px;
-      margin-bottom: 1rem;
-   }
-
-   .product-container {
       display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-      grid-gap: 10px;
-      padding: 0 10px;
-   }
-
-   .product {
-      border-radius: 10px;
-      border: var(--pry-border);
-      transition: transform 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
-      margin-bottom: 20px;
-      padding: 1px;
-   }
-
-   .product:hover {
-      box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
-      transform: translateY(-5px);
-   }
-
-   .product-container .product img {
+      grid-template-columns: 3fr 1fr;
+      height: 100vh;
       width: 100%;
-      height: 50vh;
-      object-fit: cover;
-      border-radius: 5px;
-      transition: filter 0.3s ease-in-out;
-      filter: grayscale(1);
+      background-color: var(--background2);
    }
 
-   .product-container .product:hover img {
-      filter: brightness(0.9);
+   .left {
+      flex: 1;
+      gap: 10px;
+      overflow-y: scroll;
+      scroll-snap-type: y mandatory;
+      border-right: var(--border);
+      filter: grayscale();
    }
 
-   .product-description {
-      margin-left: 5px;
+   .right {
+      position: -webkit-sticky;
+      position: sticky;
+      top: 0;
+      width: 100%;
+      height: 100vh;
+      padding: 1rem;
+      background-color: var(--background2);
+      transition: background-color 0.3s ease;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
    }
 
-   .product-description h1 {
-      font-size: 1.7em;
+   .right h1 {
+      text-transform: uppercase;
+      text-align: left;
       letter-spacing: 1px;
-      color: var(--pry-text-color);
+      font-size: 3em;
+      color: var(--sec-text);
    }
 
-   .product-description p {
-      line-height: 1.5rem;
-      font-weight: 300;
-      color: var(--pry-text-color);
-      font-size: 0.9em;
+   .right p {
+      font-size: .8em;
+      line-height: 2em;
+      color: var(--sec-text);
    }
 
-   @media only screen and (max-width: 1024px) {
-      .header {
-         padding: 0 10px;
+   .box {
+      height: 100vh;
+      scroll-snap-align: start;
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      border: var(--border);
+   }
+
+   @media (max-width: 1024px) {
+      .container {
+         display: flex;
+         flex-direction: column;
+         height: 100vh;
       }
 
-      .header h1 {
-         font-size: 3em;
+      .right {
+         position: relative;
+         padding-top: 2rem;
+         height: fit-content;
+         border: 2px solid #fff;
       }
 
-      .header p {
-         padding: 0;
-         font-size: .8em;
-         text-align: center;
-         font-weight: 400;
-         line-height: 2em;
+      .right h1 {
+         font-size: 2em;
+         margin-bottom: .3em;
       }
-.product-container{
-   display: flex;
-   flex-direction: column;
-}
-      .product-description h1 {
-         font-size: 1.4em;
-      }
-      .product-description p{
-         font-size: .7em;
+
+      .box {
+         background-size: contain;
       }
    }
 </style>
