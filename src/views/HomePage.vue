80023d26ed81217a01c5df467a713fb7b843091f
utibe inyangetuk <inyangutibz@gmail.com>
2024-10-16 16:15:48 +0100

    Homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="container">
-      <section class="hero">
+      <div class="hero">
          <div class="hero-content">
-            <h1>Welcome to Our Company</h1>
+            <h1>Welcome to COBEF</h1>
             <p>
                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
                a recusandae quis dolore velit mollitia dignissimos, consectetur
@@ -10,82 +10,118 @@
                minima quam nostrum repellat eveniet culpa?
             </p>
          </div>
-      </section>
+      </div>
 
-      <section class="section about" id="about">
-         <div class="section-content">
-            <h2>About Us</h2>
-            <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-               Distinctio voluptatum optio possimus eaque, dolor corrupti
-               dignissimos sunt reiciendis provident laboriosam, laborum nihil
-               nemo ex! Praesentium ab expedita eaque pariatur et.
-            </p>
-            <router-link to="about" class="btn-link"> Explore </router-link>
-         </div>
-      </section>
+      <div class="sections-wrapper">
+         <section class="section about" id="about">
+            <div class="section-content">
+               <h2>About Us</h2>
+               <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Distinctio voluptatum optio possimus eaque, dolor corrupti
+                  dignissimos sunt reiciendis provident laboriosam, laborum nihil
+                  nemo ex! Praesentium ab expedita eaque pariatur et.
+               </p>
+               <router-link to="about" class="btn-link"> Explore </router-link>
+            </div>
+         </section>
 
-      <section class="section product" id="products">
-         <div class="section-content">
-            <h2>Our Products</h2>
-            <p>
-               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
-               similique laudantium distinctio debitis quia, unde sequi harum.
-               Quisquam debitis aspernatur culpa labore beatae reprehenderit.
-               Mollitia velit odit cum inventore quis?
-            </p>
-            <router-link to="products" class="btn-link">
-               View products
-            </router-link>
-         </div>
-      </section>
+         <section class="section product" id="products">
+            <div class="section-content">
+               <h2>Our Products</h2>
+               <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+                  similique laudantium distinctio debitis quia, unde sequi harum.
+                  Quisquam debitis aspernatur culpa labore beatae reprehenderit.
+                  Mollitia velit odit cum inventore quis?
+               </p>
+               <router-link to="products" class="btn-link">
+                  View products
+               </router-link>
+            </div>
+         </section>
 
-      <section class="section project" id="projects">
-         <div class="section-content">
-            <h2>Our Projects</h2>
-            <p>
-               Explore the groundbreaking projects we've completed worldwide.
-            </p>
-            <router-link to="projects" class="btn-link">
-               View projects
-            </router-link>
-         </div>
-      </section>
+         <section class="section project" id="projects">
+            <div class="section-content">
+               <h2>Our Projects</h2>
+               <p>
+                  Explore the groundbreaking projects we've completed worldwide.
+               </p>
+               <router-link to="projects" class="btn-link">
+                  View projects
+               </router-link>
+            </div>
+         </section>
 
-      <section class="section careers" id="careers">
-         <div class="section-content">
-            <h2>Careers</h2>
-            <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-               quae voluptatibus provident nemo, similique iusto vero sed
-               dolores cumque inventore explicabo natus est molestias esse sequi
-               quo enim magnam facere!
-            </p>
-            <router-link to="careers" class="btn-link">
-               Get started
-            </router-link>
-         </div>
-      </section>
+         <section class="section careers" id="careers">
+            <div class="section-content">
+               <h2>Careers</h2>
+               <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                  quae voluptatibus provident nemo, similique iusto vero sed
+                  dolores cumque inventore explicabo natus est molestias esse sequi
+                  quo enim magnam facere!
+               </p>
+               <router-link to="careers" class="btn-link">
+                  Get started
+               </router-link>
+            </div>
+         </section>
 
-      <section class="section contact" id="contact">
-         <div class="section-content">
-            <h2>Contact Us</h2>
-            <p>
-               Reach out to us for more information on our services and
-               solutions.
-            </p>
-            <router-link to="contact" class="btn-link">
-               Leave us a message
-            </router-link>
-         </div>
-      </section>
+         <section class="section contact" id="contact">
+            <div class="section-content">
+               <h2>Contact Us</h2>
+               <p>
+                  Reach out to us for more information on our services and
+                  solutions.
+               </p>
+               <router-link to="contact" class="btn-link">
+                  Leave us a message
+               </router-link>
+            </div>
+         </section>
+      </div>
    </div>
 </template>
+
 <style scoped>
    .container {
       scroll-behavior: smooth;
-      background-color: var(--background2);
-      margin-top: 60px;
+      background-color: #ffffff;
+      margin-top: 40px;
+   }
+
+   .hero {
+      height: 90vh;
+      background: url("../assets/homepage-images/home.jpg") no-repeat center center/cover;
+      display: flex;
+      align-items: last baseline;
+      position: relative;
+   }
+
+   .hero-content {
+      color: #fff;
+      text-align: left;
+      margin-bottom: 100px;
+      margin-left: 50px;
+   }
+
+   .hero-content h1 {
+      text-transform: uppercase;
+   }
+
+   .hero-content p {
+      width: 60%;
+      font-size: .9em;
+      line-height: 2em;
+      font-weight: bold;
+   }
+
+   .sections-wrapper {
+      padding: 30px;
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
    }
 
    .about {
@@ -108,26 +144,12 @@
       background: url("../assets/homepage-images/contact2.jpg") no-repeat center center/cover;
    }
 
-   .hero {
-      height: 100vh;
-      background: url("../assets/homepage-images/home.jpg") no-repeat center center/cover;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-   }
-
-   .hero-content {
-      text-align: center;
-      color: #fff;
-   }
-
-   h1 {
+   .section h1 {
       font-size: 3rem;
       margin: 0;
    }
 
-   p {
+   .section p {
       font-size: 1.2rem;
    }
 
@@ -139,8 +161,9 @@
       color: var(--sec-text);
       opacity: 0;
       transform: translateY(20px);
-      transition: opacity 1s ease-in-out, transform 1s ease-in-out;
+      transition: opacity .5s ease-in-out, transform .5s ease-in-out;
       position: relative;
+      border-radius: 15px;
    }
 
    .section-content {
@@ -148,13 +171,14 @@
    }
 
    .section-content h2 {
-      font-size: 4em;
+      font-size: 3em;
       text-transform: uppercase;
-      letter-spacing: 2px;
+      letter-spacing: 1px;
    }
 
    .section-content p {
       font-size: 1em;
+      font-weight: 400;
       padding: 0 15em;
    }
 
@@ -167,20 +191,20 @@
       display: inline-flex;
       align-items: center;
       margin-top: 1rem;
-      padding: 0.8rem 2rem;
-      background-color: var(--background1);
+      padding: 0.6rem 2rem;
+      background-color: #ffffff;
       color: var(--pry-text);
-      font-size: 2rem;
-      letter-spacing: 2px;
-      text-transform: uppercase;
-      font-weight: 700;
+      font-size: .8rem;
+      letter-spacing: 1px;
+      font-weight: 600;
+      text-transform: capitalize;
       position: relative;
-      box-shadow: var(--box-shadow);
       transition: 1s ease-in-out;
+      border-radius: 5px;
    }
 
    .btn-link:hover {
-      border-radius: 20px;
+      background-color: rgba(198, 197, 197, 0.81);
       transition: 1s ease-in-out;
    }
 
@@ -189,12 +213,30 @@
    }
 
    @media only screen and (max-width: 1024px) {
+      .sections-wrapper {
+         padding: 10px;
+      }
+
       .section-content p {
          padding: 0 6em;
       }
 
       .btn-link {
          padding: 0.8rem 2rem;
+      }
+
+      .hero-content {
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         margin-left: 0;
+      }
+
+      .hero-content p,
+      h1 {
+         text-align: left;
+         width: 100%;
+         margin-left: 10px;
       }
    }
 
@@ -237,9 +279,8 @@
             });
          },
          { threshold: 0.1 }
-      ); // Adjust threshold as needed
+      );
 
-      // Observe each section
       document
          .querySelectorAll(".section")
          .forEach((section) => observer.observe(section));
