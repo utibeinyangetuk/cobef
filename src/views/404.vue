66f4f58f2ec80b25a27380fc25bf13efed3294ec
utibe inyangetuk <inyangutibz@gmail.com>
2024-10-19 21:41:20 +0100

    hmmmm

--- src/views/404.vue
+++ src/views/404.vue
@@ -4,48 +4,35 @@
       <router-link to="/">Go Home</router-link>
    </div>
 </template>
-<script setup>
-   // import { onMounted } from 'vue';
-   // import { useRouter } from 'vue-router';
-   // const router = useRouter();
 
-   // onMounted(() => {
-   //    setTimeout(() => {
-   //       router.push('/');
-   //    }, 3000);
-   // });
-</script>
 <style scoped>
    .container {
-      height: 100vh;
+      height: 85dvh;
       margin-top: 40px;
       display: flex;
       flex-direction: column;
-      justify-content: center;
-      align-items: center;
+      padding: 10px 30px;
    }
 
    p {
-      font-size: 2em;
-      text-transform: uppercase;
+      font-size: 1em;
+      text-transform: capitalize;
       margin-bottom: 10px;
-      letter-spacing: 2px;
+      letter-spacing: 1px;
    }
 
    a {
       border: var(--border);
+      border-radius: 5px;
       padding: 10px;
+      width: fit-content;
       color: var(--pry-text);
-      font-size: 1.4em;
-      border-radius: 3px;
-      font-weight: bold;
-      transition: 1s ease-in-out;
+      font-size: .7em;
       letter-spacing: 2px;
    }
 
    a:hover {
       background: var(--background2);
       color: #fff;
-      transition: 1s ease-in-out;
    }
 </style>
